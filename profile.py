fa6bfa486a75a36938d42f98ae7e0e921433d1da
malowee <32024370+malowee@users.noreply.github.com>
2018-05-15 09:15:05 -0500

    Add files via upload

--- profile.py
+++ profile.py
@@ -1,14 +1,24 @@
+import geni.portal as portal
+import geni.rspec.pg as rspec
+import geni.rspec.igext as IG
 
-<rspec xmlns:client="http://www.protogeni.net/resources/rspec/ext/client/1" xmlns:emulab="http://www.protogeni.net/resources/rspec/ext/emulab/1" xmlns:jacks="http://www.protogeni.net/resources/rspec/ext/jacks/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.geni.net/resources/rspec/3" xsi:schemaLocation="http://www.geni.net/resources/rspec/3 http://www.geni.net/resources/rspec/3/request.xsd" type="request">
-  <rspec_tour xmlns="http://www.protogeni.net/resources/rspec/ext/apt-tour/1">
-    <description type="text">This profile provides a baseline Ubuntu 14.4 image</description>
-  </rspec_tour>
-  <node client_id="basenode" exclusive="true">
-    <sliver_type name="raw">
-      <disk_image name="urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU14-64-STD"/>
-    </sliver_type>
-    <services>
-      <execute shell="/bin/sh" command="touch test.txt"/>
-    </services>
-  </node>
-</rspec>
+pc = portal.Context()
+request = rspec.Request()
+
+tourDescription = "This profile provides a baseline Ubuntu 14.4 image"
+
+#
+# Setup the Tour info with the above description and instructions.
+#  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+
+node = request.RawPC("basenode")
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU14-64-STD"
+
+node.addService(rspec.Execute(shell="/bin/sh",
+                              command="touch test.txt"))   
+
+# Print the RSpec to the enclosing page.
+portal.context.printRequestRSpec(request)
